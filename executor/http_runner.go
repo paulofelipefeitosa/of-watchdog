2b30f14ed3b333c28afd51ba8d10c773ebe70810
paulofelipefeitosa <paulofelipe.feitosa@gmail.com>
2019-12-02 17:56:43 -0300

    Add read file last line method to read CRIU restore logs

--- executor/http_runner.go
+++ executor/http_runner.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"io/ioutil"
 	"log"
+	"math"
 	"net"
 	"net/http"
 	"net/url"
@@ -30,6 +31,8 @@ type HTTPFunctionRunner struct {
 	Client         *http.Client
 	UpstreamURL    *url.URL
 	BufferHTTPBody bool
+	RestoreTime    int
+	CRIUExec       bool
 }
 
 // Start forks the process used for processing incoming requests
@@ -155,6 +158,12 @@ func (f *HTTPFunctionRunner) Run(req FunctionRequest, contentLength int64, r *ht
 	copyHeaders(w.Header(), &res.Header)
 
 	w.Header().Set("X-Duration-Seconds", fmt.Sprintf("%f", time.Since(startedTime).Seconds()))
+	if f.CRIUExec {
+		if f.RestoreTime == -1 {
+			//TODO
+		}
+		w.Header().Set("X-Restore-Time", fmt.Sprintf("%d", f.RestoreTime))
+	}
 
 	w.WriteHeader(res.StatusCode)
 	if res.Body != nil {
@@ -170,6 +179,54 @@ func (f *HTTPFunctionRunner) Run(req FunctionRequest, contentLength int64, r *ht
 	log.Printf("%s %s - %s - ContentLength: %d", r.Method, r.RequestURI, res.Status, res.ContentLength)
 
 	return nil
+}
+
+func getRestoreTime(restoreLogFilepath string) int {
+	lastLine := readLastLine(restoreLogFilepath)
+}
+
+func readLastLine(restoreLogFilepath string) string {
+	f, fLen, err := openFile(restoreLogFilepath)
+	if err != nil {
+		return ""
+	}
+	bufLen := int64(512)
+	buf := make([]byte, bufLen)
+	var line bytes.Buffer
+	for fPointer, count := fLen, 0; fPointer >= 0 && count < 2; fPointer -= bufLen {
+		offset := fPointer - bufLen
+		readBytes, err := f.ReadAt(buf, int64(math.Min(0, float64(offset))))
+		if offset < 0 { // Do not read the same content twice
+			readBytes = int(fLen % bufLen)
+		}
+		if err != nil {
+			log.Printf("Cannot read %s file seeked at %d, operation received %v", restoreLogFilepath, int64(math.Min(0, float64(offset))), err.Error())
+			break
+		}
+		i := readBytes - 1
+		for ; i >= 0 && count < 2; i-- {
+			if buf[i] == byte(10) || buf[i] == byte(13) { // if is a new line
+				count++
+			}
+		}
+		line.Write(buf[i:readBytes])
+	}
+	return line.String()
+}
+
+func openFile(filepath string) (*os.File, int64, error) {
+	f, err := os.Open(filepath)
+	if err != nil {
+		log.Printf("Cannot open %s file, operation received %v", filepath, err.Error())
+		return nil, 0, err
+	}
+	fStat, err := os.Stat(filepath)
+	if err != nil {
+		log.Printf("Cannot get %s file statistics, operation received %v", filepath, err.Error())
+		return nil, 0, err
+	}
+	fLen := fStat.Size()
+	return f, fLen, nil
 }
 
 func copyHeaders(destination http.Header, source *http.Header) {
